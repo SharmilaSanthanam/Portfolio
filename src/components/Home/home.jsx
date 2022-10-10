0bb159113b810c50398d4c6cf5510f3fbd53d146
SharmilaSanthanam <sharmieee15@gmail.com>
2022-10-10 18:54:17 +0530

    update

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -7,40 +7,35 @@ import Mern from "../../images/webdev.jpg";
 
 
 
-const Home = () => { 
+const Home = () => {
   return (
     <div className="i">
       <div className="i-left">
         <div className="i-left-wrapper">
-       
+
           <h2 className="i-intro">Hi, Myself</h2>
           <h1 className="i-name">Sharmila</h1>
-          <Nav.Link as={Link} to="/Resumepdf" className="resume">My Resume</Nav.Link>
-          {/* <a href={Resume} download>RESUME</a> */}
-          
-          
-         
-          </div>
-         
-         
-          <div className="i-title">
-            <div className="i-title-wrapper">
-              <div className="i-title-item">Mern Full-Stack Web Developer</div><br></br><br></br>
-              {/* <div className="i-title-item">UI/UX Designer</div><br></br> */}
-              <div className="i-title-item">Photo Editor</div>
-              <img
-           src={Mern}
-           
-            alt=""
-            className="imag1"
-          /> 
-           
-          </div>
-         
+          <Nav.Link as={Link} to="/Resumepdf" className="resume">Click to view the Resume!!!</Nav.Link>
+          <img
+              src={Mern}
+
+              alt=""
+              className="imag1"
+            />
         </div>
-        
+
+
+        <div className="i-title">
+          <div className="i-title-wrapper">
+            <div className="i-title-item">Mern Full-Stack Web Developer</div><br></br><br></br>
+           
+
+          </div>
+
+        </div>
+
       </div>
-      
+
     </div>
   );
 };
