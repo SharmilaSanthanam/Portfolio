14b0b9778c532a1bda06c2fc578a11094a2d6c44
SharmilaSanthanam <sharmieee15@gmail.com>
2022-10-11 22:43:31 +0530

    changes

--- src/data.js
+++ src/data.js
@@ -1,8 +1,9 @@
+
 export const products = [
     {
       id: 1,
-      img: "https://images6.alphacoders.com/330/330109.jpg",
-      link: "https://renturbook.netlify.app/",
+      img: "image/loginfront.png",
+      link: "https://renturbook.netlify.app/"
     }
     
   ];
