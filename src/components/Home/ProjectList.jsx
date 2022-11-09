2ca77e5d7bdfe2c6647e3e0854e667596eb7f7a4
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2022-11-09 15:46:35 +0530

    Update ProjectList.jsx

--- src/components/Home/ProjectList.jsx
+++ src/components/Home/ProjectList.jsx
@@ -1,6 +1,6 @@
 import "./projectList.css";
 import Project from "../Projects/project";
-import { products } from "../../data";
+import { projects } from "../../data";
 
 const ProjectList = () => {
  return (
@@ -8,14 +8,24 @@ const ProjectList = () => {
       <div className="pl-texts">
         <h1 className="pl-title">Create & inspire!!!</h1>
         <p className="pl-desc">
-          Have a glance at the project...
+          Have a glance at the project by clicking on the below image...
         </p>
       </div>
       <div className="pl-list">
-        {products.map((item) => (
+        {projects.map((item) => (
           <Project key={item.id} img={item.img} link={item.link} />
         ))}
+             <div className="pl-texts">
+        <p className="pl-desc">
+        <a href={"https://github.com/SharmilaSanthanam/RentUrBookFrontend"} download style={{color:"green"}}>Frontend source code</a>
+       &nbsp;  &nbsp;
+    
+        <a href={"https://github.com/SharmilaSanthanam/RentUrBookBackend"} download style={{color:"green"}}>Backend source code</a> 
+        
+        </p>
+        </div>
       </div>
+     
     </div>
   );
 };
